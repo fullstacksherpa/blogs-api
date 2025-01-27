191d335b63c67678df3f83929a99d31c1ecf341d
Ongchen Sherpa <fullstacksherpa@gmail.com>
2025-01-27 05:53:08 -0800

    migration created with migrate

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -50,6 +50,9 @@ func main() {
 		log.Panic(err)
 	}
 
+	defer db.Close()
+	log.Println("database connection pool established")
+
 	store := store.NewStorage(db)
 
 	app := &application{
--- scripts/db_init.sql
+++ scripts/db_init.sql
@@ -1,1 +1,9 @@
 CREATE DATABASE social;
+
+
+
+//initialize migrate
+migrate create -seq -ext sql -dir ./cmd/migrate/migrations create_users
+
+//doing migration up
+migrate -path=./cmd/migrate/migrations -database="postgres://admin:adminpassword@localhost:5433/socialnetwork?sslmode=disable" up
